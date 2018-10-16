f9e3d8e0763e0cdbff533d98b252c59483e08949
mis0b1gboy <37484559+mis0b1gboy@users.noreply.github.com>
2018-10-16 12:07:21 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get update -y && sudo DEBIAN_FRONTEND=no
 sudo DEBAIN_FRONTEND=noninteractive apt-get install software-properties-common -y
 sudo add-apt-repository ppa:certbot/certbot -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -y 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx python python-certbot-nginx 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx python-certbot-nginx python 
 sudo mv nginx.conf /etc/nginx/nginx.conf
 sudo nginx
+sudo certbot -n --nginx --redirect --rsa-key-size 2048 --agree-tos --email Jons-tech-support@pm.me --no-eff-email -d jonstechsupport.com
